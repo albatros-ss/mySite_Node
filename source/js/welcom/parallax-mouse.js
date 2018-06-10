2afcf0d0833f019e9d788a3d8a26e711177d2596
Vadim Volodko <albatros.andr@gmail.com>
2018-06-10 18:19:34 +0300

    update

---
+++ source/js/welcom/parallax-mouse.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const parallaxMouse = (function () {
+    function moveParallax(e) {
+        let img = document.getElementById('bg'),
+        initialX = (window.innerWidth / 2) - e.pageX,
+        initialY = (window.innerHeight / 2) - e.pageY,
+        divider = 150;
+        img.style.transform = `translate3d(${initialX / divider}px, ${initialY / divider}px, 0)`;
+    };
+    return {
+        init: () => window.addEventListener('mousemove', moveParallax)
+    }
+})();
+export default parallaxMouse;

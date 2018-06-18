ee79e850594136fd92cbc5fa02b055690c14af04
Vadim Volodko <albatros.andr@gmail.com>
2018-06-18 12:33:55 +0300

    update

--- gulp/paths/js.foundation.js
+++ gulp/paths/js.foundation.js
@@ -2,5 +2,6 @@
 
 module.exports = [
     './node_modules/svg4everybody/dist/svg4everybody.min.js',
-    './node_modules/jquery/dist/jquery.min.js'
+    './node_modules/jquery/dist/jquery.min.js',
+    './node_modules/@ckeditor/ckeditor5-build-classic/build/ckeditor.js'
 ];
